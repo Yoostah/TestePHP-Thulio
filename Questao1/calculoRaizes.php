0ec80f1f81e84d70c7ba2153634f3bb810d2cb61
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2020-01-05 12:06:44 -0300

    condicional para raizes iguais

--- Questao1/calculoRaizes.php
+++ Questao1/calculoRaizes.php
@@ -10,7 +10,9 @@ function calculoRaizes($a, $b=0, $c=0){
   } else {
     $x1 = (-$b + sqrt ($delta)) / (2 * $a);
     $x2 = (-$b - sqrt ($delta)) / (2 * $a);
-        
+
+    if($delta == 0) return $x1.' é a única raiz para a equação pois o valor de delta é zero!';
+
     return 'As raizes são '.$x1.' e '.$x2;
   }
 }
