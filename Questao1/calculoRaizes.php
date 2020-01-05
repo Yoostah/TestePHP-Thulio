1d53acf1bc01b62c7b630489eeb381cd8c006b70
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2020-01-05 12:06:17 -0300

    verificação se a equação de segundo grau e inicializacao caso a mesma seja incompleta. condicionais para calculo de raizes

--- Questao1/calculoRaizes.php
+++ Questao1/calculoRaizes.php
@@ -1,13 +1,21 @@
 <?php
 
-function calculoRaizes($a, $b, $c = 0){
+function calculoRaizes($a, $b=0, $c=0){
+  if ($a == 0) return 'Não é uma equação do segundo grau!';
+
   $delta = ($b*$b)-((4*$a)*$c);
-  $x1 = (-$b + sqrt ($delta)) / (2 * $a);
-  $x2 = (-$b - sqrt ($delta)) / (2 * $a);
-  return 'As raizes são '.$x1.' e '.$x2;
+
+  if ($delta < 0) {
+    return 'Não existem raizes para a equação pois o valor de delta é negativo!';
+  } else {
+    $x1 = (-$b + sqrt ($delta)) / (2 * $a);
+    $x2 = (-$b - sqrt ($delta)) / (2 * $a);
+        
+    return 'As raizes são '.$x1.' e '.$x2;
+  }
 }
 
-echo calculoRaizes(1, 0, 6);
+echo calculoRaizes(5, -1, 0);
 
 ?>
 
